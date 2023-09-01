0502034c2e58b709d9ae3ca0f2e2c52eeb2955f2
e-caliano <e.caliano2@gmail.com>
2023-09-01 16:31:19 +0200

    Update server1.py
    inizio implementazione della classe server

--- server1.py
+++ server1.py
@@ -1,69 +1,76 @@
-# -*- coding: utf-8 -*-
-"""
-Implementazione del server 1 
-"""
 import socket
 import threading
+import sys
 
-def gestisci_connessione(client_socket, client_address):
-    """
-    # Logica per gestire la connessione dal load balancer
-    # N.B. Implementa qui la logica per ricevere e gestire le richieste
-    # inviate dal load balancer al server
+class server(object):
+    def __init__(self):
+        self.ip = "127.0.0.1"
+        self.port = 5002
+        self.clients = []
+        self.active_clients = []
+        self.richieste = {}  # la chiave è ip del client, argomento nome richieste
+        self.comandi = {}
 
-    Parameters
-    ----------
-    client_socket
-    client_address 
 
-    Returns
-    -------
-    None.
 
-    """
-    while True:
-        data = client_socket.recv(4096)
-        if not data:
-            break
-        # Esegui le operazioni richieste dal load balancer
-        # e invia le risposte al load balancer
-        response = "Risposta del server"
-        client_socket.sendall(response.encode())
-    
-    client_socket.close()
-    
-def start_server(host, port):
-    """
-    Funzione che crea la socket del server e lo mette in ascolto sulla porta
-    del load balancer
+    def socket_server(self):
+        """
+        Funzione che crea la socket del server e crea un thread che rimane in ascolto per ricevere i comandi dal load balancer
+        """
+        # creo una socket server
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # collego la socket al server
+        server_socket.bind((self.ip, self.port))
+        # metto in ascolto il server
+        server_socket.listen()
+        print(f"Server in ascolto su {self.ip}:{self.port}")
+        # creo un thread che rimane in ascolto per ricevere i comandi dal load balancer
+        thread_gestione_client = threading.Thread(target=self.gestione_client, args=(server_socket,))
+        thread_gestione_client.start()
+        thread_gestione_client.join()
+        server_socket.close()
 
-    Parameters
-    ----------
-    host 
-    port 
-        
-    Returns
-    -------
-    None.
-    """
-    # Creo la socket del server 
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # Collego la socket del server all'IP e alla porta dell'host (quindi del Load Balancer)
-    server_socket.bind((host, port))
-    # Il server è in ascolto; può avere massimo 10 richieste in attesa
-    server_socket.listen()
-    print(f"Server in ascolto su {host}:{port}")
 
-    while True:
-        client_socket, client_address = server_socket.accept()
-        print(f"Connessione accettata da {client_address[0]}:{client_address[1]}")
-        
-        # Avvia un thread separato per gestire la connessione
-        connection_thread = threading.Thread(target=gestisci_connessione, args=(client_socket, client_address))
-        connection_thread.start()
+    def gestione_client(self, server_socket):
+        """
+        Funzione che rimane in ascolto per ricevere i comandi dal load balancer
+        """
+        while True:
+            # accetto le connessioni in entrata
+            client_socket, client_ip = server_socket.accept()
+            # avvio un thread per gestire le richieste del client
+            thread_richieste_client = threading.Thread(target=self.richieste_client, args=(client_socket,))
+            thread_richieste_client.start()
+            # se il client si disconnette, termina il thread
+            if client_socket not in self.clients:
+                thread_richieste_client.join()
 
-host ="127.0.0.1"
-port = 5003
 
-start_server(host, port)
+    def richieste_client(self, client_socket):
+        """
+        Funzione che gestisce le richieste del client
+        """
+        while True:
+            # ricevo i comandi dal load balancer
+            data = client_socket.recv(1024)
+            # se non ricevo più comandi, esco dal ciclo
+            if not data:
+                break
+            # decodifico i comandi
+            comando = data.decode("utf-8")
+            # inserisco i comandi nella lista dei comandi da svolgere
+            self.richieste[client_socket] = comando
+            # richiamo il metodo per eseguire i comandi
+            self.esegui_comandi(comando, client_socket)
+        client_socket.close()
+
+
+    def esegui_comandi(self, comando, client_socket):
+        pass
+
+    # MANCA L'ATTRIBUTO CHE INVIA LA SOLUZIONE DEI COMANDI AL CLIENT
+
+
+
 
